9f3792cc297e7dd6812b9b7535136540ca51b9eb
jedidiah oyeyemi <ooyeye2@uic.edu>
2013-07-02 11:54:31 -0500

    new rx

--- RX_RADIO_ARDUNIO/RX_RADIO_ARDUNIO/RX_RADIO_ARDUNIO.ino
+++ RX_RADIO_ARDUNIO/RX_RADIO_ARDUNIO/RX_RADIO_ARDUNIO.ino
@@ -1,10 +1,8 @@
-
-
 #include <SPI.h>
 #include <RADIO.h>
  
  
- byte RX_Buffer[61]={0};
+ byte RX_Buffer[10]={0};
 byte size,i;
 
  
@@ -12,7 +10,7 @@ byte size,i;
  void setup()
 {
   Serial.begin (9600);
-//  Serial.println ();
+  Serial.println ("start");
   
      
 
@@ -23,7 +21,7 @@ byte size,i;
   SPI.setDataMode(SPI_MODE0) ;
   CC110L.GDO_Set();
   CC110L.Reset();
-  //CC110L.RegConfig();
+  CC110L.RegConfig();
   //CC110L.PATABLE();
   CC110L.SetReceive();
   
@@ -37,12 +35,11 @@ void loop()
   if(CC110L.CheckReceiveFlag())
   {
     size=CC110L.ReceiveData(RX_Buffer);
-  Serial.print("size=");
-  Serial.println(size);
-    for(i=0;i<20;i++)
+    Serial.println(size);
+    for(i=0;i<10;i++)
     {
       Serial.print(RX_Buffer[i],DEC);
-//      Serial.print(' ',BYTE);
+     Serial.print(' ');
     }
     Serial.println("");
     CC110L.SetReceive();
